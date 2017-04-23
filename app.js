2a9ba01a8a4d764fe22d4e4d9da2e53d2cf06efe
Michał Mazurek <michalgmazurek@gmail.com>
2017-04-23 11:01:22 +0200

    Almost there

--- app.js
+++ app.js
@@ -1,154 +1,191 @@
-document.addEventListener("DOMContentLoaded", function () {
-	var self;
-	var Furry = function (x, y) {
-		this.x = x;
-		this.y = y;
-		this.direction = "right";
-	}
-	var Coin = function () {
-		this.x = Math.floor(Math.random() * 10);
-		this.y = Math.floor(Math.random() * 10);
-	}
-	var Game = function () {
-		self = this;
-		this.sounds = [];
-		this.furry = new Furry(0, 0);
-		this.coin = new Coin();
-		this.board = document.querySelectorAll("#board div");
-		this.score = 0;
-		this.scoreBoard = document.querySelector("#score span");
-		this.levels();
-		this.generator();
-		this.control();
-		this.soundInit();
-		this.sounds.bg.play();
-	}
-	Game.prototype.soundInit = function () {
-		var sounds = document.querySelectorAll("audio");
-		this.sounds.finish = sounds[0];
-		this.sounds.coin = sounds[1];
-		this.sounds.coin.volume = 0.6;
-		this.sounds.bg = sounds[2];
-		this.sounds.bg.volume = 0.15;
-	}
-	Game.prototype.generator = function () {
-		for (var i = 0; i < this.board.length; i++) {
-			this.board[i].classList.remove("furry");
-		}
-		var furryBox = this.furry.x + this.furry.y * 10;
-		if (this.board[furryBox] != undefined) {
-			this.board[furryBox].classList.add("furry");
-		}
-		var coinBox = this.coin.x + this.coin.y * 10;
-		this.board[coinBox].classList.add("coin");
-	};
-	Game.prototype.control = function () {
-		document.addEventListener("keydown", function (event) {
-			if (event.keyCode === 39) {
-				self.furry.direction = "right";
-				return;
-			}
-			else if (event.keyCode === 37) {
-				self.furry.direction = "left";
-				return;
-			}
-			else if (event.keyCode === 38) {
-				self.furry.direction = "up";
-				return;
-			}
-			else if (event.keyCode === 40) {
-				self.furry.direction = "down";
-				return;
-			}
-		});
-	}
-	Game.prototype.oneMove = function () {
-		if (self.furry.direction === "right") {
-			self.furry.x++;
-		}
-		else if (self.furry.direction === "left") {
-			self.furry.x--;
-		}
-		else if (self.furry.direction === "up") {
-			self.furry.y--;
-		}
-		else if (self.furry.direction === "down") {
-			self.furry.y++;
-		}
-		self.wall();
-		self.collision();
-		self.generator();
-	}
-	Game.prototype.wall = function () {
-		if (self.furry.x < 0 || self.furry.x > 9 || self.furry.y < 0 || self.furry.y > 9) {
-			document.getElementById("board").classList.add("hidden");
-			clearInterval(this.interval);
-			this.sounds.finish.currentTime = 0;
-			this.sounds.finish.play();
-			document.querySelector(".gameOver").classList.remove("hidden");
-			setTimeout(function(){
-				document.querySelector(".gameOver").classList.add("hidden");
-				document.querySelector(".yourScore").classList.remove("hidden");
-				self.addScore();
-			}, 3000);
-		}
-		
-	}
-	Game.prototype.collision = function () {
-		if ((self.furry.x + (self.furry.y * 10)) === (self.coin.x + (self.coin.y * 10))) {
-			this.board[this.coin.x + this.coin.y * 10].classList.remove("coin");
-			this.coin = new Coin;
-			this.score++;
-			this.scoreBoard.innerHTML = this.score;
-			this.levels();
-			this.sounds.coin.currentTime = 0;
-			this.sounds.coin.play();
-		}
-	}
-	Game.prototype.levels = function () {
-		var level = this.score;
-		clearInterval(this.interval);
-		this.interval = setInterval(this.oneMove, 300 - (level * 15));
-	}
-	Game.prototype.addScore = function () {
-		var scoreToAdd = document.getElementById("scoreToAdd");
-		scoreToAdd.innerHTML = this.score;
-		
-//		var database = firebase.database();
-//		var url = "https://first-game-4ca11.firebaseio.com/";
-		var firebaseRef = new Firebase("https://first-game-4ca11.firebaseio.com/scores");
-		var submitButton = document.querySelector(".button");
-		
-		function submitScore() {
-			var name = document.getElementById("nameInput").value;
-			var score = scoreToAdd;
-			var dbRefName = firebase.database().ref().child("name");
-		}
+document.addEventListener("DOMContentLoaded", function() {
+    var self;
+	var playerScore;
+    var Furry = function(x, y) {
+        this.x = x;
+        this.y = y;
+        this.direction = "right";
+    }
+    var Coin = function() {
+        this.x = Math.floor(Math.random() * 10);
+        this.y = Math.floor(Math.random() * 10);
+    }
+    var Game = function() {
+        self = this;
+        this.sounds = [];
+        this.furry = new Furry(0, 0);
+        this.coin = new Coin();
+        this.board = document.querySelectorAll("#board div");
+        this.score = 0;
+        this.scoreBoard = document.querySelector("#score span");
+        this.levels();
+        this.generator();
+        this.control();
+        this.soundInit();
+        this.sounds.bg.play();
+    }
+    Game.prototype.soundInit = function() {
+        var sounds = document.querySelectorAll("audio");
+        this.sounds.finish = sounds[0];
+        this.sounds.coin = sounds[1];
+        this.sounds.coin.volume = 0.6;
+        this.sounds.bg = sounds[2];
+        this.sounds.bg.volume = 0.15;
+    }
+    Game.prototype.generator = function() {
+        for (var i = 0; i < this.board.length; i++) {
+            this.board[i].classList.remove("furry");
+        }
+        var furryBox = this.furry.x + this.furry.y * 10;
+        if (this.board[furryBox] != undefined) {
+            this.board[furryBox].classList.add("furry");
+        }
+        var coinBox = this.coin.x + this.coin.y * 10;
+        this.board[coinBox].classList.add("coin");
+    };
+    Game.prototype.control = function() {
+        document.addEventListener("keydown", function(event) {
+            if (event.keyCode === 39) {
+                self.furry.direction = "right";
+                return;
+            } else if (event.keyCode === 37) {
+                self.furry.direction = "left";
+                return;
+            } else if (event.keyCode === 38) {
+                self.furry.direction = "up";
+                return;
+            } else if (event.keyCode === 40) {
+                self.furry.direction = "down";
+                return;
+            }
+        });
+    }
+    Game.prototype.oneMove = function() {
+        if (self.furry.direction === "right") {
+            self.furry.x++;
+        } else if (self.furry.direction === "left") {
+            self.furry.x--;
+        } else if (self.furry.direction === "up") {
+            self.furry.y--;
+        } else if (self.furry.direction === "down") {
+            self.furry.y++;
+        }
+        self.wall();
+        self.collision();
+        self.generator();
+    }
+    Game.prototype.wall = function() {
+        if (self.furry.x < 0 || self.furry.x > 9 || self.furry.y < 0 || self.furry.y > 9) {
+            document.getElementById("board").classList.add("hidden");
+            clearInterval(this.interval);
+            this.sounds.finish.currentTime = 0;
+            this.sounds.finish.play();
+            document.querySelector(".gameOver").classList.remove("hidden");
+            setTimeout(function() {
+                document.querySelector(".gameOver").classList.add("hidden");
+                document.querySelector(".yourScore").classList.remove("hidden");
+                self.addScore();
+            }, 3000);
+        }
 
-		submitButton.onclick = submitScore();
-	}
+    }
+    Game.prototype.collision = function() {
+        if ((self.furry.x + (self.furry.y * 10)) === (self.coin.x + (self.coin.y * 10))) {
+            this.board[this.coin.x + this.coin.y * 10].classList.remove("coin");
+            this.coin = new Coin;
+            this.score++;
+            this.scoreBoard.innerHTML = this.score;
+            this.levels();
+            this.sounds.coin.currentTime = 0;
+            this.sounds.coin.play();
+        }
+    }
+    Game.prototype.levels = function() {
+        var level = this.score;
+        clearInterval(this.interval);
+        this.interval = setInterval(this.oneMove, 300 - (level * 15));
+    }
+    Game.prototype.addScore = function() {
+        var scoreToAdd = document.getElementById("scoreToAdd");
+        scoreToAdd.innerHTML = this.score;
+		playerScore = this.score;
 
-	function start() {
-		var mariano = document.getElementById("avatarContainer");
-		var gameBody = document.getElementById("gameBody");
-	
-		document.addEventListener("keydown", function spaceStart (event) {
-			if (event.keyCode === 32) {
-				var startGame = document.getElementById("startGame");
-				startGame.classList.add("hidden");
-				var startGame = new Game;
-				gameBody.classList.remove("hidden");
-				document.removeEventListener("keydown", spaceStart);
-			}
-		});
-//		mariano.addEventListener("click", function () {
-//			var startGame = document.getElementById("startGame");
-//			startGame.classList.add("hidden");
-//			var startGame = new Game;
-//			gameBody.classList.remove("hidden");
-//			document.removeEventListener("keydown", spaceStart);
-//		});
-		
-	}
-	start();
+        //		var database = firebase.database();
+        //		var url = "https://first-game-4ca11.firebaseio.com/";
+        // var firebaseRef = new Firebase("https://first-game-4ca11.firebaseio.com/scores");
+        // var submitButton = document.querySelector(".button");
+		//
+        // function submitScore() {
+        //     var name = document.getElementById("nameInput").value;
+        //     var score = scoreToAdd;
+        //     var dbRefName = firebase.database().ref().child("name");
+        // }
+		//
+        // submitButton.onclick = submitScore();
+    }
+
+    function start() {
+        var mariano = document.getElementById("avatarContainer");
+        var gameBody = document.getElementById("gameBody");
+
+        document.addEventListener("keydown", function spaceStart(event) {
+            if (event.keyCode === 32) {
+                var startGame = document.getElementById("startGame");
+                startGame.classList.add("hidden");
+                var startGame = new Game;
+                gameBody.style.display = "flex";
+                document.removeEventListener("keydown", spaceStart);
+            }
+        });
+        //		mariano.addEventListener("click", function () {
+        //			var startGame = document.getElementById("startGame");
+        //			startGame.classList.add("hidden");
+        //			var startGame = new Game;
+        //			gameBody.classList.remove("hidden");
+        //			document.removeEventListener("keydown", spaceStart);
+        //		});
+
+    }
+    start();
+
+	var addScoreBtn = document.getElementById("addScoreBtn");
+
+    addScoreBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        var userNameInputValue = document.getElementById("nameInput").value;  // $("#nameInput").val();
+
+		console.log(userNameInputValue);
+		console.log(score);
+
+        firebase.database().ref().push({
+            userName: userNameInputValue,
+            score: playerScore
+        }, function() {
+            var topScores = firebase.database().ref().orderByChild('score').limitToLast(10).on('value', function(fbdata) {
+                var myDatabaseObject = fbdata.exportVal();
+                var keys = Object.keys(myDatabaseObject);
+                var myResults = [];
+                var yourScoreSection = document.querySelector(".yourScore");
+                var topTenSection = document.getElementById("topTenSection");
+                var topTenScores = document.getElementById("topTenScores");
+
+                keys.forEach(function(key) {
+                    var data = myDatabaseObject[key]
+                    myResults.push(data);
+                });
+
+                var sortedObjectsArray = myResults.sort(function(a, b) {
+                    return b.score - a.score
+                });
+                yourScoreSection.style.display = "none";
+                topTenSection.style.display = "flex";
+
+                for (var i = 0; i < sortedObjectsArray.length; i++) {
+                    var newLi = document.createElement("li");
+                    topTenScores.appendChild(newLi);
+                    newLi.innerHTML = (i + 1) + ".&nbsp;" + sortedObjectsArray[i].userName + "&nbsp;:&nbsp;" + sortedObjectsArray[i].score;
+                }
+            });
+        });
+    });
 });
